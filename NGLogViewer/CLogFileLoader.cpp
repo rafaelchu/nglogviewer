06d101b5c4501d985071a52c6b189e01b2138f4c
deryann <deryann@3e024c04-234c-0410-9b90-ef5fda9c747a>
2008-05-04 10:05:11 +0000

    [How] Implement: vector<int> GetVecIntProcessNumber(); vector<wstring> GetVecWstrTags();

--- NGLogViewer/CLogFileLoader.cpp
+++ NGLogViewer/CLogFileLoader.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int CLogFileLoader::PreProcessing()
 {
+	this->ClearAll();
+
 	wifstream wiFile(this->m_wstrFilename.c_str());
 	if (!wiFile)
 	{
@@ -14,12 +16,15 @@ int CLogFileLoader::PreProcessing()
 	}
 
 	wchar_t wszLineBuffer[LINE_BUFFER_SIZE];
-
+	class CLineBuffer *pCLineBuffer = new CLineBuffer();
 	while(wiFile.getline(wszLineBuffer, LINE_BUFFER_SIZE))
 	{
-		wstring wstr=GetTag(wszLineBuffer);
-		m_setWstrTags.insert(wstr);
+		GetLineBufferData(wszLineBuffer, pCLineBuffer);
+		m_setWstrTags.insert(pCLineBuffer->m_wstrMessage);
+		m_setIntProcessNumber.insert(pCLineBuffer->m_nProcess);
 	}
+
+	delete pCLineBuffer;
 	return 0;
 }
 
@@ -47,13 +52,68 @@ CLogFileLoader::~CLogFileLoader()
 
 void CLogFileLoader::PrintInfo()
 {
-	hash_set<wstring>::iterator hsIter;
+	//List All Tags
+	vector<wstring>::iterator vecWstrIter;
 	int i =0;
-	for ( hsIter = m_setWstrTags.begin(); hsIter != m_setWstrTags.end(); hsIter++ )
+	vector<wstring> vecWstrTags=GetVecWstrTags();
+	for ( vecWstrIter = vecWstrTags.begin(); vecWstrIter != vecWstrTags.end(); ++vecWstrIter )
 	{
 		i++;
 		wchar_t *wstrA;
-		wstrA =(wchar_t *) (hsIter->c_str());
+		wstrA =(wchar_t *) (vecWstrIter->c_str());
 		wprintf(L"%d:%s\n", i, wstrA);
 	}
+
+	//List All Process
+	vector<int> vecProcessNumber= this->GetVecIntProcessNumber();
+	vector<int>::iterator veciIter;
+	i=0;
+	for (veciIter = vecProcessNumber.begin(); veciIter != vecProcessNumber.end(); ++veciIter)
+	{
+		i++;
+		wprintf(L"%d:[%d]\n", i, *veciIter);
+	}
+
+}
+
+int CLogFileLoader::GetLineBufferData(wchar_t *wszBuffer, class CLineBuffer *pCLineBuffer)
+{
+	wchar_t wszSkip[LINE_BUFFER_SIZE]={0};
+	wchar_t wszString[LINE_BUFFER_SIZE]={0};
+	swscanf(wszBuffer, L"%d %f [%d] %s", &pCLineBuffer->m_nLineNumber, &pCLineBuffer->m_Time, &pCLineBuffer->m_nProcess, wszString);
+	if (wszString[0] !='[')
+		wszString[0] = '\0';
+	pCLineBuffer->m_wstrMessage = wszString;
+	return 0;
+}
+
+vector<int> CLogFileLoader::GetVecIntProcessNumber()
+{
+	vector<int> vecIntProcessNumber;
+	set<int>::iterator sIter;
+	int i =0;
+	for ( sIter = m_setIntProcessNumber.begin(); sIter != m_setIntProcessNumber.end(); ++sIter )
+	{
+		vecIntProcessNumber.push_back(*sIter);
+	}
+	return vecIntProcessNumber;
+}
+
+vector<wstring> CLogFileLoader::GetVecWstrTags()
+{
+	vector<wstring> vecWstrTags;
+	hash_set<wstring>::iterator hsIter;
+	int i =0;
+	for ( hsIter = m_setWstrTags.begin(); hsIter != m_setWstrTags.end(); ++hsIter )
+	{
+		vecWstrTags.push_back(*hsIter);
+	}
+	return vecWstrTags;
+}
+
+int CLogFileLoader::ClearAll()
+{
+	m_setIntProcessNumber.clear();
+	m_setWstrTags.clear();
+	return 0;
 }
