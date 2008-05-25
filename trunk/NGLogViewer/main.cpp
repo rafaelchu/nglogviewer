f2a24403dae042c1c2972ca9ded15a70c3851ecf
deryann <deryann@3e024c04-234c-0410-9b90-ef5fda9c747a>
2008-05-25 12:55:19 +0000

    [How] Implement CRegSetting class 1. Read/Write DWORD 2. Read/Write wchar_t *

--- trunk/NGLogViewer/main.cpp
+++ trunk/NGLogViewer/main.cpp
@@ -1,6 +1,7 @@
 
 #include "CLogFileLoader.h"
 #include "sattime.h"
+#include "..\Utility\CRegSetting.h"
 using namespace std;
 
 #ifdef WIN32
@@ -11,7 +12,7 @@ using namespace stdext;
 using namespace __gnu_cxx;
 #endif
 
-int main (int argc, char **argv)
+int TestCLogFileLoader()
 {
 	wstring wstr(L"C:\\all.LOG");
 	sattime cTimeCheck;
@@ -23,41 +24,61 @@ int main (int argc, char **argv)
 	cTimeCheck.printSecs("CLogFileLoader preprocessing");
 	//print all tags
 	//cLogFileLoader.PrintInfo();
-	
+
 	//Test case:
 	//cLogFileLoader.SetLineNumberFilter(10000, 20000);
-	
+
 	//cLogFileLoader.SetTimeFilter(cLogFileLoader.GetMinTime()+20.0, cLogFileLoader.GetMinTime()+40.0);
-	
+
 	//Test case: filter process
 	/*
-		vector<int> vec;
-		vec.clear();
-		vec.push_back(532);
-		vec.push_back(2756);
-		vec.push_back(1968);
-		cLogFileLoader.SetProcessFilter(&vec);
+	vector<int> vec;
+	vec.clear();
+	vec.push_back(532);
+	vec.push_back(2756);
+	vec.push_back(1968);
+	cLogFileLoader.SetProcessFilter(&vec);
 	*/
 
 	//Test case: filter [CLRec4]
-		//vector<wstring> vec2;
-		//vec2.push_back(L"[CLSchMgr]");
-		//vec2.push_back(L"[CLRec4]");
-		//vec2.push_back(L"EPG");
-		//vec2.push_back(L"(SQL)");
-		//cLogFileLoader.SetTagsFilter(&vec2);
+	//vector<wstring> vec2;
+	//vec2.push_back(L"[CLSchMgr]");
+	//vec2.push_back(L"[CLRec4]");
+	//vec2.push_back(L"EPG");
+	//vec2.push_back(L"(SQL)");
+	//cLogFileLoader.SetTagsFilter(&vec2);
 
 	//Test case: Exclude
 	//cLogFileLoader.SetKeyWordExcludeFilter(L"[CLSchMgr];[CLRec4];EPG");
 
 	//Test case: Include
 	cLogFileLoader.SetKeyWordIncludeFilter(L"[CLRec4];EPG");
-	
+
 	cTimeCheck.restart();
 	cLogFileLoader.RunFilterResult();
 	cTimeCheck.printSecs("CLogFileLoader RunFilterResult ");
 	//cLogFileLoader.PrintResult();
 	cLogFileLoader.SaveResultAs(L"C:\\Result.txt");
+	return 0;
+}
+
+int TestCRegSetting()
+{
+	class CRegSetting cRegSetting(L"SOFTWARE\\NG1\\BBB");
+	DWORD a=0;
+	cRegSetting.WriteRegKey(L"SettingA", 5);
+	cRegSetting.ReadRegKey(L"SettingA", a);
+	wchar_t wstr[128] = {0};
+	cRegSetting.WriteRegKey(L"SettingString", L"abcd");
+	cRegSetting.ReadRegKey(L"SettingString", 128, wstr);
+	return 0;
+}
+
+
+int main (int argc, char **argv)
+{
+	//TestCLogFileLoader();
+	TestCRegSetting();
 	system("PAUSE");
 	return 0;
 }
---
+++ trunk/Utility/CRegSetting.h
@@ -0,0 +1,22 @@
+
+#ifndef _CREGSETTING_H_
+#define _CREGSETTING_H_
+#include <Windows.h>
+class CRegSetting
+{
+    public:
+    CRegSetting(const wchar_t *wszPath, HKEY hKeyRoot = HKEY_CURRENT_USER);
+	~CRegSetting();	
+
+	bool WriteRegKey(const wchar_t *wszName, DWORD dwInput);
+	bool WriteRegKey(const wchar_t *wszName, const wchar_t *wszValue);
+
+	bool ReadRegKey(const wchar_t *wszName, DWORD &dwInput);
+	bool ReadRegKey(const wchar_t *wszName, int nSize, wchar_t *wszValue);
+
+
+    private:
+		HKEY 	m_hKey;
+
+};
+#endif
