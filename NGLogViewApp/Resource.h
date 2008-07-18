66abc00612c832ab6f36e9b16aab032bd3deed2c
chianing.chang <chianing.chang@3e024c04-234c-0410-9b90-ef5fda9c747a>
2008-07-18 03:56:33 +0000

    [How]: Change the background of bookmark icon to transparent.

--- NGLogViewApp/Resource.h
+++ NGLogViewApp/Resource.h
@@ -9,6 +9,8 @@
 #define IDR_NGLogViewAppTYPE            129
 #define IDR_ACCELERATOR1                130
 #define IDB_BOOKMARK                    132
+#define IDI_BOOKMARK                    135
+#define IDI_BOOKMARK1                   140
 #define IDC_EDIT_INCLUDE                1000
 #define IDC_EDIT_EXCLUDE                1001
 #define IDC_CHECK_EMPTY_STRING          1002
@@ -17,13 +19,11 @@
 #define IDC_EDIT_HIGHLIGHT              1005
 #define IDC_COMBOBOXEX1                 1006
 #define IDC_COMBO_HIGHLIGHT_EDIT        1009
-#define IDC_EDIT2                       1010
 #define ID_EDIT_FONT                    32771
 #define ID_EDIT_PROPERTIES              32772
 #define HOT_KEY_FIND_DLG                32773
 #define HOT_KEY_SET_BOOKMARK            32774
 #define HOT_KEY_FIND_STRING             32775
-#define ID_ACCELERATOR32776             32776
 #define HOT_KEY_FIND_NEXT_BOOKMARK      32776
 #define HOT_KEY_OPEN_PROPERTY_PAGE      32778
 
@@ -31,9 +31,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_RESOURCE_VALUE        141
 #define _APS_NEXT_COMMAND_VALUE         32780
 #define _APS_NEXT_CONTROL_VALUE         1011
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
