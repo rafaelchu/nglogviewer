6229fad15d3c60f16984ce44fa259dec8555b8f5
deryann <deryann@3e024c04-234c-0410-9b90-ef5fda9c747a>
2008-08-08 07:56:36 +0000

    [How] add BG/FG Setting in highlight filter.

--- NGLogViewApp/Resource.h
+++ NGLogViewApp/Resource.h
@@ -20,6 +20,9 @@
 #define IDC_COMBOBOXEX1                 1006
 #define IDC_COMBO_HIGHLIGHT_EDIT        1009
 #define IDC_BOOKMARK_ALL                1011
+#define IDC_BUTTON_BG_COLOR             1012
+#define IDC_BUTTON_BG_COLOR2            1013
+#define IDC_BUTTON_FG_COLOR             1013
 #define IDD_NGFINDDIALOG                1540
 #define ID_EDIT_FONT                    32771
 #define ID_EDIT_PROPERTIES              32772
@@ -36,7 +39,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        141
 #define _APS_NEXT_COMMAND_VALUE         32781
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
